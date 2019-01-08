51692885016c9f52899d4c4050b05bde7610c56b
Kumaran Baskaran <kbaskaran2@wisc.edu>
2019-01-08 14:28:53 -0600

    setup file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from pybmrb import __version__
+
+setup(name='pybmrb',
+      version=__version__,
+      author='Kumaran Baskaran',
+      author_email='kbaskaran@bmrb.wisc.edu',
+      description='PyBMRB provides tools to visualize chemical shift data in BMRB',
+      keywords=['bmrb', 'hsqc', 'chemical shift', 'nmrstar', 'biomagresbank', 'biological magnetic resonance bank'],
+      url='https://github.com/uwbmrb/PyBMRB',
+      licence='MIT')
