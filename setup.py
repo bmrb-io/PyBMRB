8a4775deb60f39878703d783fb9c41c8e558bfec
Kumaran Baskaran <kumar.physics@gmail.com>
2021-07-10 15:41:46 -0400

    set up and release notes updated for release

--- setup.py
+++ setup.py
@@ -20,12 +20,12 @@ long_des = open('README.rst', 'r').read()
 
 setup(name='pybmrb',
       version=get_version(),
-      packages=['pybmrb2'],
+      packages=['pybmrb'],
       install_requires=['pandas','requests>=2.21.0,<=3','plotly>=4.1.0','pynmrstar>=3.0.4','numpy>1.15'],
       python_requires='>=3.6',
       author='Kumaran Baskaran',
       author_email='baskaran@uchc.edu',
-      description='PyBMRB provides tools for visializing BMRB entries and  NMR-STAR files. '
+      description='PyBMRB provides tools for visualizing BMRB entries and  NMR-STAR files. '
                   'Maintained by the BMRB.',
       long_description=long_des,
       long_description_content_type='text/x-rst',
@@ -35,11 +35,8 @@ setup(name='pybmrb',
                 'Histogram'],
       url='https://github.com/uwbmrb/PyBMRB',
       license='MIT',
-      package_data={'pynmrstar': ['reference_files/schema.csv',
-                                  'reference_files/comments.str',
-                                  'reference_files/data_types.csv']},
       classifiers=[
-          'Development Status :: 3 - Alpha',
+          'Development Status :: 5 - Production/Stable',
           'Environment :: Console',
           'Programming Language :: Python :: 3 :: Only',
           'Programming Language :: Python :: 3.6',
