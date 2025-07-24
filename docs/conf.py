72860ff6dd258f8666b584192138fbfa5e43a5f1
Kumaran Baskaran <kumar-physics@users.noreply.github.com>
2025-07-24 17:06:40 -0400

    Update conf.py
    read the docs config updated

--- docs/conf.py
+++ docs/conf.py
@@ -74,3 +74,8 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+# python3 -m sphinx.ext.intersphinx https://docs.python-requests.org/en/master/objects.inv
+intersphinx_mapping = {
+    'requests': ('https://docs.python-requests.org/en/master/', None),
+    'python': ('https://docs.python.org/3', None)
+}
