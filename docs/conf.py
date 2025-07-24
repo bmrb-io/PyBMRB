35d34e28e86adf5fb777564ba377b23ec5f3a461
Kumaran Baskaran <kumar-physics@users.noreply.github.com>
2025-07-24 17:53:43 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # html_theme_options = {
 #     'analytics_id': 'G-CH3JS5076R',  # Provided by Google in your dashboard
