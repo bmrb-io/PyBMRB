4b991d1d07b3b579d94a3f36ff1381406827448a
Kumaran Baskaran <kumar-physics@users.noreply.github.com>
2025-07-24 17:30:15 -0400

    Update conf.py
    read the docs theme updated

--- docs/conf.py
+++ docs/conf.py
@@ -52,8 +52,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
+# html_theme = 'sphinx_rtd_theme'
 # html_theme_options = {
 #     'analytics_id': 'G-CH3JS5076R',  # Provided by Google in your dashboard
 #     'analytics_anonymize_ip': False,
